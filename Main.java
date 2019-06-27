ef3858f35d3a9163bb65995d8ddf500f73f2877b
MarinaChvek <52216049+MarinaChvek@users.noreply.github.com>
2019-06-27 21:26:38 +0300

    Меню

--- Main.java
+++ Main.java
@@ -1,12 +1,50 @@
 package com.company;
-
-public class Main {
+import java.util.Scanner;
+public class Main1 {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int click =1;
+         while(click !=0) {
+            System.out.println("Введите действие от 0 до 9");
+            click=in.nextInt();
+            switch (click) {
+                case 0:
+                    break;
+                case 1:
 
-        for (int i=20; i<36; i++){
-            System.out.println(i);
+                    System.out.println("Выволнение действия1");
+                    break;
+                case 2:
+                    System.out.println("Выволнение действия2");
+                    break;
+                case 3:
+                    System.out.println("Выволнение действия3");
+                    break;
+                case 4:
+                    System.out.println("Выволнение действия4");
+                    break;
+                case 5:
+                    System.out.println("Выволнение действия5");
+                    break;
+                case 6:
+                    System.out.println("Выволнение действия6");
+                    break;
+                case 7:
+                    System.out.println("Выволнение действия7");
+                    break;
+                case 8:
+                    System.out.println("Выволнение действия8");
+                    break;
+                case 9:
+                    System.out.println("Выволнение действия9");
+                    break;
+                default:
+                    System.out.println("Действие не выбрано");
+            }
+
+            }
+         in.close();
         }
-	// write your code here
+
     }
-}
